f363f1e3e43f30078c1a4cf2b850a3528383f0ec
Sanemat <o.gata.ken@gmail.com>
2016-12-25 14:56:49 +0900

    :shirt: Fix by eslint

--- bin/build-dev.js
+++ bin/build-dev.js
@@ -4,10 +4,8 @@ const pify = require('pify');
 const rimraf = require('rimraf');
 const cpy = require('cpy');
 
-pify(rimraf)('docs').then(result => {
-  return cpy(['src/index.html'], 'docs');
-}).then(result => {
-  console.log('It works fine.')
-}).catch(result => {
+pify(rimraf)('docs').then(result => cpy(['src/index.html'], 'docs')).then((result) => {
+  console.log('It works fine.');
+}).catch((result) => {
   console.error(result);
 });
