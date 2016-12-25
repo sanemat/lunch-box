565d1ea8d5388bae24b709f212ea112c2df7eeea
Sanemat <o.gata.ken@gmail.com>
2016-12-25 13:12:54 +0900

    :up: Add build settings

--- bin/build-dev.js
+++ bin/build-dev.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
 
+const pify = require('pify');
+const rimraf = require('rimraf');
+
+pify(rimraf)('docs').then(result => {
+  console.log('It works fine.')
+}).catch(result => {
+  console.error(result);
+});
