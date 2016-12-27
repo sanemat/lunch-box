13b7911eb7546d742e6a4510520afb6963dbc916
Sanemat <o.gata.ken@gmail.com>
2016-12-27 19:11:59 +0900

    :metal: Add image loader

--- bin/build-dev.js
+++ bin/build-dev.js
@@ -6,7 +6,7 @@ const pify = require('pify');
 const rimraf = require('rimraf');
 const cpy = require('cpy');
 
-pify(rimraf)('docs').then(() => cpy(['src/index.html', 'src/*.png'], 'docs')).then(() => {
+pify(rimraf)('docs').then(() => cpy(['src/index.html', 'src/dokaben-blueback.png'], 'docs')).then(() => {
   console.log('It works fine.');
 }).catch((result) => {
   console.error(result);
